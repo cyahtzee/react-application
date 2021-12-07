6df652eefcc2989f6f8fbab2d683c5d319ef461c
Konstantin Yatsenko <moahtdeep@gmail.com>
2021-12-08 00:51:01 +0900

    created first class component

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
 
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
-};
+// const Hello = ({ name }) => {
+//   return (
+//     <div>
+//       Hello,
+//       {name}
+//     </div>
+//   );
+// };
+
+class Hello extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      clicked: false
+    };
+  }
+
+  handleCLick = () => {
+    // change the state
+    this.setState({
+      clicked: !this.state.clicked
+    });
+  }
+
+  render() {
+    return (
+      <div className={this.state.clicked ? "clicked" : null}
+        onClick={this.handleCLick}>Hello {this.props.name}</div>
+    )
+  }
+}
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
+  ReactDOM.render(
+    <Hello name="Kostya">
+      <Hello name="World" />
+    </Hello>, root);
 }
